4808b0a61208df23ec8c79fd9929b0eaeb0ce43d
“jahjam” <elijahjamesuk@hotmail.com>
2023-09-02 17:49:22 +0100

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import styles from "./App.module.css";
 
+import Header from "./feature/Header/Header.tsx";
+
 function App() {
     return (
         <>
             <main className={styles.container}>
+                <Header />
             </main>
         </>
     )
