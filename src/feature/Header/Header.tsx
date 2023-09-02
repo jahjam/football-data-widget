5814f875126d2a8c0055dccc9baae3b4f6be4e03
“jahjam” <elijahjamesuk@hotmail.com>
2023-09-02 17:50:08 +0100

    create header component with background image

---
+++ src/feature/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+function Header() {
+    return (
+        <header className={styles.container}>
+            <div className={styles.mask}/>
+        </header>
+    )
+}
+
+export default Header;
