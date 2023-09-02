09406fffa8ba122cec87ab6daa0b5ddfaf07d079
“jahjam” <elijahjamesuk@hotmail.com>
2023-09-02 22:09:12 +0100

    make fetch to format API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
 import styles from "./App.module.css";
 
 import Header from "./feature/Header/Header.tsx";
+import {useRequest} from "./hooks/use-request.tsx";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [homeTeam, setHomeTeam] = useState<Team>();
+    const [awayTeam, setAwayTeam] = useState<Team>();
+    const [curMatch, setCurMatch] = useState<CurrentMatch>();
+    const [liveData, setLiveData] = useState<LiveData>();
+
+    const {sendRequest, isLoading, isError} = useRequest();
+
+    useEffect(() => {
+        const res = (data: Match) => {
+            const homeTeam: Contestant | undefined = data.contestant.find((team) => team.position === "home");
+            const awayTeam: Contestant | undefined = data.contestant.find((team) => team.position === "away");
+
+            setHomeTeam({
+                contestant: homeTeam,
+                stats: data.liveData.lineups.home.stats,
+                goals: data.liveData.matchDetails.scores.total.home
+            });
+            setAwayTeam({
+                contestant: awayTeam,
+                stats: data.liveData.lineups.away.stats,
+                goals: data.liveData.matchDetails.scores.total.away
+            });
+            setCurMatch({
+                match: data.meta.competition,
+            });
+            setLiveData(data.liveData);
+        }
+
+        sendRequest(res);
+    }, [sendRequest]);
+
     return (
         <>
-            <main className={styles.container}>
-                <Header />
-            </main>
+            {isLoading &&  <span>Loading...</span>}
+
+            {isError ?
+                <span>Something went wrong!</span>
+                :
+                <main className={styles.container}>
+                    <Header awayTeam={awayTeam} homeTeam={homeTeam} curMatch={curMatch} liveData={liveData}/>
+                </main>
+            }
+
         </>
     )
 }
