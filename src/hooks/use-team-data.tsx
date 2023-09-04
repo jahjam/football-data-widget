580be8def2faf01e746f429d9246a372d675a1bd
“jahjam” <elijahjamesuk@hotmail.com>
2023-09-04 08:44:04 +0100

    format team data

--- src/hooks/use-team-data.tsx
+++ src/hooks/use-team-data.tsx
@@ -2,18 +2,33 @@ import {useEffect, useState} from 'react';
 
 import {useRequest} from "./use-request.tsx";
 
+type TeamData = {
+    homeTeamPlayers: (First11 | Sub)[],
+    awayTeamPlayers: (First11 | Sub)[],
+    combinedTotalShotsPerAllPlayers: (First11 | Sub)[],
+    combinedTotalPassers: (First11 | Sub)[],
+    combinedTotalTacklers: (First11 | Sub)[],
+}
+
 export const useTeamData = () => {
     const [homeTeam, setHomeTeam] = useState<Team>();
     const [awayTeam, setAwayTeam] = useState<Team>();
     const [curMatch, setCurMatch] = useState<CurrentMatch>();
     const [liveData, setLiveData] = useState<LiveData>();
+    const [teamData, setTeamData] = useState<TeamData>();
 
     const {sendRequest, isLoading, isError} = useRequest();
 
+    // None of the iterations in here are very performant at all and with more time I would look to optimise these
     useEffect(() => {
         const res = (data: Match) => {
+            if (!data) return;
+
+            // keeping all home / away team data separate in case need to use individual team data separately from one another in future
             const homeTeam: Contestant | undefined = data.contestant.find((team) => team.position === "home");
             const awayTeam: Contestant | undefined = data.contestant.find((team) => team.position === "away");
+            const homeTeamPlayers = [...data.liveData.lineups.home.first11, ...data.liveData.lineups.home.subs];
+            const awayTeamPlayers = [...data.liveData.lineups.away.first11, ...data.liveData.lineups.away.subs];
 
             const homeTeamScorers = data.liveData?.goal.filter(player => player.contestantId === homeTeam?.id);
             const awayTeamScorers = data.liveData?.goal.filter(player => player.contestantId === awayTeam?.id);
@@ -21,6 +36,27 @@ export const useTeamData = () => {
             const awayTeamYellowCards = data.liveData?.card.filter(player => player.contestantId === awayTeam?.id && player.type === "YC");
             const homeTeamRedCards = data.liveData?.card.filter(player => player.contestantId === homeTeam?.id && (player.type === "Y2C" || player.type === "RC"));
             const awayTeamRedCards = data.liveData?.card.filter(player => player.contestantId === awayTeam?.id && player.type === "Y2C" || player.type === "RC");
+            const totalHomeTeamShots = homeTeamPlayers.filter(player => {
+                return player.stats.shots > 0;
+            });
+            const totalAwayTeamShots = awayTeamPlayers.filter(player => {
+                return player.stats.shots > 0;
+            });
+            const totalHomeTeamPassers = homeTeamPlayers.filter(player => {
+                return player.stats.passes > 0;
+            });
+            const totalAwayTeamPassers = awayTeamPlayers.filter(player => {
+                return player.stats.passes > 0;
+            });
+            const totalHomeTeamTacklers = homeTeamPlayers.filter(player => {
+                return player.stats.tackles > 0;
+            });
+            const totalAwayTeamTacklers = awayTeamPlayers.filter(player => {
+                return player.stats.tackles > 0;
+            });
+            const combinedTotalShotsPerAllPlayers = [...totalHomeTeamShots, ...totalAwayTeamShots];
+            const combinedTotalPassers = [...totalHomeTeamPassers, ...totalAwayTeamPassers];
+            const combinedTotalTacklers = [...totalHomeTeamTacklers, ...totalAwayTeamTacklers];
 
             setHomeTeam({
                 contestant: homeTeam,
@@ -42,6 +78,16 @@ export const useTeamData = () => {
                 match: data.meta.competition,
             });
             setLiveData(data.liveData);
+
+            setTeamData(
+                {
+                    homeTeamPlayers,
+                    awayTeamPlayers,
+                    combinedTotalShotsPerAllPlayers,
+                    combinedTotalPassers,
+                    combinedTotalTacklers
+                }
+            );
         }
 
         sendRequest(res);
@@ -52,6 +98,7 @@ export const useTeamData = () => {
         awayTeam,
         curMatch,
         liveData,
+        teamData,
         isLoading,
         isError
     }
